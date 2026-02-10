05fa6a3da3d81ff3c1f72ae6cdef67b42e04703d
Derrick Bommarito <negentropicdev@gmail.com>
2026-02-10 15:23:18 -0800

    fix fmtlog cmake deficiencies in tree so can build wihtout changes on new clones

--- cmake/fmtlog.cmake
+++ cmake/fmtlog.cmake
@@ -8,3 +8,16 @@ FetchContent_Declare(fmtlog
     EXCLUDE_FROM_ALL
     SYSTEM)
 FetchContent_MakeAvailable(fmtlog)
+
+#the fmtlog repo is not setup to integrate into a higher level CMake config
+# so some additional details need to be filled in.
+target_include_directories(fmtlog-static
+    PUBLIC
+        ${fmtlog_SOURCE_DIR}
+        ${fmtlog_SOURCE_DIR}/fmt/include
+)
+
+target_link_libraries(fmtlog-static
+    PUBLIC
+        fmt
+)
